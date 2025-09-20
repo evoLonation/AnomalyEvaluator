81cb245abe6c9b8f3ea6fbd2c4a85627483564e1
evolonation <zhaozhengyang@buaa.edu.cn>
2025-09-20 10:51:33 +0800

    1. 完善掩码图转换 2. 完善DetectionDataset设计（category有序）

--- evaluation2.py
+++ evaluation2.py
@@ -184,6 +184,7 @@ class Detector:
 
 @dataclass
 class CategoryData:
+    category: str
     image_paths: list[str]
     correct_labels: list[bool]
     mask_paths: list[str | None]
@@ -196,16 +197,20 @@ class DetectionDataset:
     def __init__(
         self,
         name: str,
-        category_data: dict[str, CategoryData],
+        category_datas: list[CategoryData],
     ):
         self.name = name
-        self.category_data = category_data
+        self.category_datas = category_datas
+        self.category2data = {data.category: data for data in category_datas}
+        assert len(self.category2data) == len(
+            category_datas
+        ), "Duplicate category names found."
 
     @jaxtyped(typechecker=typechecker)
     def generate_masks(
         self, category: str, image_shape: tuple[int, int], start: int = 0, end: int = -1
     ) -> Bool[np.ndarray, "N H W"]:
-        mask_paths = self.category_data[category].mask_paths
+        mask_paths = self.category2data[category].mask_paths
         if end == -1:
             end = len(mask_paths)
         masks = []
@@ -213,14 +218,17 @@ class DetectionDataset:
             if mask_path is None:
                 masks.append(np.zeros(image_shape, dtype=bool))
                 continue
-            img_mask = Image.open(mask_path).convert("1")
+            img_mask = Image.open(mask_path).convert("L")
+            img_mask = (np.array(img_mask) > 0).astype(np.uint8) * 255
+            img_mask = Image.fromarray(img_mask, mode="L")
             # size: (W, H)
             if img_mask.size != (image_shape[1], image_shape[0]):
                 # 对correct_masks进行resize, 类似下面的处理方式
                 img_mask = img_mask.resize(
-                    (image_shape[1], image_shape[0]), Image.Resampling.NEAREST
+                    (image_shape[1], image_shape[0]), Image.Resampling.BILINEAR
                 )
             img_mask = np.array(img_mask)
+            img_mask = img_mask > 127  # 二值化
             masks.append(img_mask)
         return np.array(masks, dtype=bool)
 
@@ -246,7 +254,7 @@ class MVTecAD(DetectionDataset):
             "zipper",
         ]
 
-        category_data = {}
+        category_datas = []
         for category in categories:
             category_path = path / category / "test"
             if not category_path.exists():
@@ -284,13 +292,16 @@ class MVTecAD(DetectionDataset):
                 correct_labels = [correct_labels[i] for i in indices]
                 mask_paths = [mask_paths[i] for i in indices]
 
-            category_data[category] = CategoryData(
-                image_paths=image_paths,
-                correct_labels=correct_labels,
-                mask_paths=mask_paths
+            category_datas.append(
+                CategoryData(
+                    category=category,
+                    image_paths=image_paths,
+                    correct_labels=correct_labels,
+                    mask_paths=mask_paths,
+                )
             )
 
-        super().__init__("MVTecAD", category_data)
+        super().__init__("MVTecAD", category_datas)
 
 
 class RealIAD(DetectionDataset):
@@ -298,8 +309,8 @@ class RealIAD(DetectionDataset):
         json_dir = path / "realiad_jsons"
         image_dir = path / "realiad_1024"
         assert json_dir.exists() and image_dir.exists()
-        
-        category_data = {}
+
+        category_datas = []
         for json_file in json_dir.glob("*.json"):
             print(f"Loading dataset from {json_file}...")
             with open(json_file, "r") as f:
@@ -307,11 +318,11 @@ class RealIAD(DetectionDataset):
             normal_class = data["meta"]["normal_class"]
             prefix: str = data["meta"]["prefix"]
             category: str = json_file.stem
-            
+
             image_paths: list[str] = []
             correct_labels: list[bool] = []
             mask_paths: list[str | None] = []
-            
+
             for item in data["test"]:
                 anomaly_class = item["anomaly_class"]
                 correct_label = anomaly_class != normal_class
@@ -333,13 +344,16 @@ class RealIAD(DetectionDataset):
                 correct_labels = [correct_labels[i] for i in indices]
                 mask_paths = [mask_paths[i] for i in indices]
 
-            category_data[category] = CategoryData(
-                image_paths=image_paths,
-                correct_labels=correct_labels,
-                mask_paths=mask_paths
+            category_datas.append(
+                CategoryData(
+                    category=category,
+                    image_paths=image_paths,
+                    correct_labels=correct_labels,
+                    mask_paths=mask_paths,
+                )
             )
 
-        super().__init__("RealIAD", category_data)
+        super().__init__("RealIAD", category_datas)
 
 
 class VisA(DetectionDataset):
@@ -348,13 +362,13 @@ class VisA(DetectionDataset):
         with open(meta_file, "r") as f:
             data = json.load(f)
 
-        category_data = {}
+        category_datas = []
         # 只使用test数据进行评估
         for category, samples in data["test"].items():
             image_paths: list[str] = []
             correct_labels: list[bool] = []
             mask_paths: list[str | None] = []
-            
+
             for sample in samples:
                 img_path = path / sample["img_path"]
                 image_paths.append(str(img_path))
@@ -376,75 +390,82 @@ class VisA(DetectionDataset):
                 correct_labels = [correct_labels[i] for i in indices]
                 mask_paths = [mask_paths[i] for i in indices]
 
-            category_data[category] = CategoryData(
-                image_paths=image_paths,
-                correct_labels=correct_labels,
-                mask_paths=mask_paths
+            category_datas.append(
+                CategoryData(
+                    category=category,
+                    image_paths=image_paths,
+                    correct_labels=correct_labels,
+                    mask_paths=mask_paths,
+                )
             )
 
-        super().__init__("VisA", category_data)
+        super().__init__("VisA", category_datas)
 
 
 def evaluation_detection(
     path: Path, detector: Detector, dataset: DetectionDataset, batch_size: int = 1
 ):
     print(f"Evaluating detector {detector.name} on dataset {dataset.name}...")
-    
-    total_samples = sum(len(data.image_paths) for data in dataset.category_data.values())
-    print(f"Dataset has {total_samples} samples across {len(dataset.category_data)} categories.")
+
+    total_samples = sum(len(data.image_paths) for data in dataset.category_datas)
+    print(
+        f"Dataset has {total_samples} samples across {len(dataset.category_datas)} categories."
+    )
 
     if not path.exists():
         path.mkdir(parents=True, exist_ok=True)
-    columns = [x for x in DetectionMetrics.__dataclass_fields__.keys()]
-    metrics_output_path = path / f"{detector.name}_{dataset.name}_metrics_macroavg.csv"
+    metrics_output_path = path / f"{detector.name}_{dataset.name}_metrics_macroavg2.csv"
+    category_metrics: list[tuple[str, DetectionMetrics]] = []
     if metrics_output_path.exists():
-        print(
-            f"Metrics for {detector.name} on {dataset.name} already exist. Skipping..."
-        )
+        print(f"Metrics for {detector.name} on {dataset.name} already exist.")
+        # add here
         return
 
-    category_metrics = {}
-    for category, data in dataset.category_data.items():
-        print(f"Evaluating category: {category}")
+    for data in dataset.category_datas:
+        print(f"Evaluating category: {data.category}")
         metrics_calculator = MetricsCalculator()
-        
-        for i in tqdm(range(0, len(data.image_paths), batch_size), desc=f"Processing {category}"):
+
+        for i in tqdm(
+            range(0, len(data.image_paths), batch_size),
+            desc=f"Processing {data.category}",
+        ):
             batch_image_paths = data.image_paths[i : i + batch_size]
             batch_correct_labels = data.correct_labels[i : i + batch_size]
             results = detector(batch_image_paths)
             ground_truth = DetectionGroundTruth(
                 true_labels=np.array(batch_correct_labels, dtype=bool),
                 true_masks=dataset.generate_masks(
-                    category=category,
-                    image_shape=results.anomaly_maps.shape[1:], 
-                    start=i, 
-                    end=i + batch_size
+                    category=data.category,
+                    image_shape=results.anomaly_maps.shape[1:],
+                    start=i,
+                    end=i + batch_size,
                 ),
             )
             metrics_calculator.update(results, ground_truth)
 
-        category_metrics[category] = metrics_calculator.compute()
-        print(f"Category {category} metrics: {category_metrics[category]}")
+        metrics = metrics_calculator.compute()
+        category_metrics.append((data.category, metrics))
+        print(f"Category {data.category} metrics: {metrics}")
 
     # 计算平均指标
     avg_metrics = {}
     for field in DetectionMetrics.__dataclass_fields__.keys():
-        values = [getattr(metrics, field) for metrics in category_metrics.values()]
+        values = [getattr(metrics, field) for _, metrics in category_metrics]
         avg_metrics[field] = np.mean(values)
 
     # 保存结果
+    columns = [x for x in DetectionMetrics.__dataclass_fields__.keys()]
     rows = []
-    for category, metrics in category_metrics.items():
+    for _, metrics in category_metrics:
         row = [getattr(metrics, col) for col in columns]
         rows.append(row)
-    
+
     # 添加平均值行
     avg_row = [avg_metrics[col] for col in columns]
     rows.append(avg_row)
-    
-    # 创建索引
-    indices = list(category_metrics.keys()) + ["Average"]
-    
+
+    indices = [category for category, _ in category_metrics] + ["Average"]
+
     table = pd.DataFrame(data=rows, columns=columns, index=indices)
     table.to_csv(metrics_output_path)
     print(f"Results saved to {metrics_output_path}")
